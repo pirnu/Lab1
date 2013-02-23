1df1136a18985e607f1acdc2e6b881ad1a420be5
pirnu <user@server.fake>
2013-02-23 14:09:09 -0500

    Project Uploaded

---
+++ CG_SK_Lab1/CG_SK_Lab1/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace CG_SK_Lab1
+{
+    partial class UEnter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AButton = new System.Windows.Forms.Button();
+            this.ANameText = new System.Windows.Forms.TextBox();
+            this.APassText = new System.Windows.Forms.TextBox();
+            this.ANameLabel = new System.Windows.Forms.Label();
+            this.APassLabel = new System.Windows.Forms.Label();
+            this.ALogin = new System.Windows.Forms.Button();
+            this.UIDText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UNameText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UPinText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ADispRep = new System.Windows.Forms.Button();
+            this.ADispPred = new System.Windows.Forms.Button();
+            this.AEnroll = new System.Windows.Forms.Button();
+            this.AChangePass = new System.Windows.Forms.Button();
+            this.AMenuLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AButton
+            // 
+            this.AButton.Location = new System.Drawing.Point(12, 210);
+            this.AButton.Name = "AButton";
+            this.AButton.Size = new System.Drawing.Size(108, 32);
+            this.AButton.TabIndex = 0;
+            this.AButton.Text = "Admin Login";
+            this.AButton.UseVisualStyleBackColor = true;
+            this.AButton.Click += new System.EventHandler(this.AButton_Click);
+            // 
+            // ANameText
+            // 
+            this.ANameText.Location = new System.Drawing.Point(13, 269);
+            this.ANameText.Name = "ANameText";
+            this.ANameText.Size = new System.Drawing.Size(138, 20);
+            this.ANameText.TabIndex = 1;
+            this.ANameText.Visible = false;
+            // 
+            // APassText
+            // 
+            this.APassText.Location = new System.Drawing.Point(13, 314);
+            this.APassText.Name = "APassText";
+            this.APassText.Size = new System.Drawing.Size(138, 20);
+            this.APassText.TabIndex = 2;
+            this.APassText.Visible = false;
+            // 
+            // ANameLabel
+            // 
+            this.ANameLabel.AutoSize = true;
+            this.ANameLabel.Location = new System.Drawing.Point(13, 249);
+            this.ANameLabel.Name = "ANameLabel";
+            this.ANameLabel.Size = new System.Drawing.Size(55, 13);
+            this.ANameLabel.TabIndex = 3;
+            this.ANameLabel.Text = "Username";
+            this.ANameLabel.Visible = false;
+            // 
+            // APassLabel
+            // 
+            this.APassLabel.AutoSize = true;
+            this.APassLabel.Location = new System.Drawing.Point(12, 296);
+            this.APassLabel.Name = "APassLabel";
+            this.APassLabel.Size = new System.Drawing.Size(53, 13);
+            this.APassLabel.TabIndex = 4;
+            this.APassLabel.Text = "Password";
+            this.APassLabel.Visible = false;
+            // 
+            // ALogin
+            // 
+            this.ALogin.Location = new System.Drawing.Point(12, 344);
+            this.ALogin.Name = "ALogin";
+            this.ALogin.Size = new System.Drawing.Size(75, 23);
+            this.ALogin.TabIndex = 5;
+            this.ALogin.Text = "Log In";
+            this.ALogin.UseVisualStyleBackColor = true;
+            this.ALogin.Visible = false;
+            this.ALogin.Click += new System.EventHandler(this.ALogin_Click);
+            // 
+            // UIDText
+            // 
+            this.UIDText.Location = new System.Drawing.Point(16, 55);
+            this.UIDText.Name = "UIDText";
+            this.UIDText.Size = new System.Drawing.Size(178, 20);
+            this.UIDText.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Scan ID";
+            // 
+            // UNameText
+            // 
+            this.UNameText.Location = new System.Drawing.Point(273, 55);
+            this.UNameText.Name = "UNameText";
+            this.UNameText.Size = new System.Drawing.Size(138, 20);
+            this.UNameText.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(273, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter Username";
+            // 
+            // UPinText
+            // 
+            this.UPinText.Location = new System.Drawing.Point(273, 112);
+            this.UPinText.Name = "UPinText";
+            this.UPinText.Size = new System.Drawing.Size(138, 20);
+            this.UPinText.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(273, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Enter Pin";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ADispRep
+            // 
+            this.ADispRep.Location = new System.Drawing.Point(273, 228);
+            this.ADispRep.Name = "ADispRep";
+            this.ADispRep.Size = new System.Drawing.Size(107, 23);
+            this.ADispRep.TabIndex = 13;
+            this.ADispRep.Text = "Display Reports";
+            this.ADispRep.UseVisualStyleBackColor = true;
+            this.ADispRep.Visible = false;
+            // 
+            // ADispPred
+            // 
+            this.ADispPred.Location = new System.Drawing.Point(273, 257);
+            this.ADispPred.Name = "ADispPred";
+            this.ADispPred.Size = new System.Drawing.Size(107, 23);
+            this.ADispPred.TabIndex = 14;
+            this.ADispPred.Text = "Display Predictions";
+            this.ADispPred.UseVisualStyleBackColor = true;
+            this.ADispPred.Visible = false;
+            // 
+            // AEnroll
+            // 
+            this.AEnroll.Location = new System.Drawing.Point(273, 286);
+            this.AEnroll.Name = "AEnroll";
+            this.AEnroll.Size = new System.Drawing.Size(107, 23);
+            this.AEnroll.TabIndex = 15;
+            this.AEnroll.Text = "Enroll New Cadet";
+            this.AEnroll.UseVisualStyleBackColor = true;
+            this.AEnroll.Visible = false;
+            // 
+            // AChangePass
+            // 
+            this.AChangePass.Location = new System.Drawing.Point(273, 314);
+            this.AChangePass.Name = "AChangePass";
+            this.AChangePass.Size = new System.Drawing.Size(107, 23);
+            this.AChangePass.TabIndex = 16;
+            this.AChangePass.Text = "Change Password";
+            this.AChangePass.UseVisualStyleBackColor = true;
+            this.AChangePass.Visible = false;
+            // 
+            // AMenuLabel
+            // 
+            this.AMenuLabel.AutoSize = true;
+            this.AMenuLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.AMenuLabel.Location = new System.Drawing.Point(273, 196);
+            this.AMenuLabel.Name = "AMenuLabel";
+            this.AMenuLabel.Size = new System.Drawing.Size(98, 20);
+            this.AMenuLabel.TabIndex = 18;
+            this.AMenuLabel.Text = "Admin Menu";
+            this.AMenuLabel.Visible = false;
+            // 
+            // UEnter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 426);
+            this.Controls.Add(this.AMenuLabel);
+            this.Controls.Add(this.AChangePass);
+            this.Controls.Add(this.AEnroll);
+            this.Controls.Add(this.ADispPred);
+            this.Controls.Add(this.ADispRep);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UPinText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UNameText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UIDText);
+            this.Controls.Add(this.ALogin);
+            this.Controls.Add(this.APassLabel);
+            this.Controls.Add(this.ANameLabel);
+            this.Controls.Add(this.APassText);
+            this.Controls.Add(this.ANameText);
+            this.Controls.Add(this.AButton);
+            this.Name = "UEnter";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AButton;
+        private System.Windows.Forms.TextBox ANameText;
+        private System.Windows.Forms.TextBox APassText;
+        private System.Windows.Forms.Label ANameLabel;
+        private System.Windows.Forms.Label APassLabel;
+        private System.Windows.Forms.Button ALogin;
+        private System.Windows.Forms.TextBox UIDText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UNameText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UPinText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ADispRep;
+        private System.Windows.Forms.Button ADispPred;
+        private System.Windows.Forms.Button AEnroll;
+        private System.Windows.Forms.Button AChangePass;
+        private System.Windows.Forms.Label AMenuLabel;
+    }
+}
+
---
+++ CG_SK_Lab1/CG_SK_Lab1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CG_SK_Lab1
+{
+    public partial class UEnter : Form
+    {
+        public UEnter()
+        {
+            InitializeComponent();
+            this.ActiveControl = UIDText;
+            
+        }
+
+        
+
+        private void AButton_Click(object sender, EventArgs e)
+        {
+            // Make admin login visible
+            ANameLabel.Visible = true;
+            ANameText.Visible = true;
+            APassLabel.Visible = true;
+            APassText.Visible = true;
+            ALogin.Visible = true;
+        }
+
+        private void ALogin_Click(object sender, EventArgs e)
+        {
+            // Validate admin
+            // Make admin menu visible
+            AMenuLabel.Visible = true;
+            ADispRep.Visible = true;
+            ADispPred.Visible = true;
+            AEnroll.Visible = true;
+            AChangePass.Visible = true;
+        }
+    }
+}
