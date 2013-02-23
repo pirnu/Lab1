5c62a94ce83454602ad17ffea05390c86bb5cfc2
pirnu <user@server.fake>
2013-02-23 14:47:29 -0500

    Added New Forms
    Enroll, Change Password, Predictions, Reports

---
+++ CG_SK_Lab1/CG_SK_Lab1/Predictions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CG_SK_Lab1
+{
+    public partial class Predictions : Form
+    {
+        public Predictions()
+        {
+            InitializeComponent();
+        }
+    }
+}
