212bcbb44571c06bb91de2d42dadd2639e10f289
ScottKenney <Scott.W.Kenney@uscga.edu>
2013-02-24 16:14:09 -0500

    Updated Change Password
    Does everything but actually changes password

--- CG_SK_Lab1/CG_SK_Lab1/ChangePassword.Designer.cs
+++ CG_SK_Lab1/CG_SK_Lab1/ChangePassword.Designer.cs
@@ -28,38 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangePassword));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.feedback = new System.Windows.Forms.Label();
             this.nameText = new System.Windows.Forms.TextBox();
             this.pinText = new System.Windows.Forms.TextBox();
+            this.verify = new System.Windows.Forms.Label();
+            this.verified = new System.Windows.Forms.TextBox();
+            this.commit = new System.Windows.Forms.Button();
+            this.nstatus = new System.Windows.Forms.PictureBox();
+            this.pstatus = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nstatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pstatus)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(60, 54);
+            this.label1.Location = new System.Drawing.Point(53, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.Size = new System.Drawing.Size(91, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Username";
+            this.label1.Text = "Username: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(60, 141);
+            this.label2.Location = new System.Drawing.Point(70, 113);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.Size = new System.Drawing.Size(70, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "New Pin";
+            this.label2.Text = "New Pin:";
             // 
             // feedback
             // 
             this.feedback.AutoSize = true;
             this.feedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.feedback.Location = new System.Drawing.Point(28, 240);
+            this.feedback.Location = new System.Drawing.Point(31, 15);
             this.feedback.Name = "feedback";
             this.feedback.Size = new System.Drawing.Size(277, 20);
             this.feedback.TabIndex = 2;
@@ -67,23 +75,78 @@
             // 
             // nameText
             // 
-            this.nameText.Location = new System.Drawing.Point(61, 91);
+            this.nameText.Location = new System.Drawing.Point(153, 70);
             this.nameText.Name = "nameText";
             this.nameText.Size = new System.Drawing.Size(130, 20);
             this.nameText.TabIndex = 3;
+            this.nameText.Leave += new System.EventHandler(this.nameText_Leave);
             // 
             // pinText
             // 
-            this.pinText.Location = new System.Drawing.Point(64, 173);
+            this.pinText.Location = new System.Drawing.Point(153, 115);
             this.pinText.Name = "pinText";
             this.pinText.Size = new System.Drawing.Size(127, 20);
             this.pinText.TabIndex = 4;
+            // 
+            // verify
+            // 
+            this.verify.AutoSize = true;
+            this.verify.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verify.Location = new System.Drawing.Point(26, 156);
+            this.verify.Name = "verify";
+            this.verify.Size = new System.Drawing.Size(114, 20);
+            this.verify.TabIndex = 5;
+            this.verify.Text = "Verify New Pin:";
+            // 
+            // verified
+            // 
+            this.verified.Location = new System.Drawing.Point(153, 156);
+            this.verified.Name = "verified";
+            this.verified.Size = new System.Drawing.Size(127, 20);
+            this.verified.TabIndex = 6;
+            this.verified.KeyDown += new System.Windows.Forms.KeyEventHandler(this.verified_KeyDown);
+            // 
+            // commit
+            // 
+            this.commit.Location = new System.Drawing.Point(92, 206);
+            this.commit.Name = "commit";
+            this.commit.Size = new System.Drawing.Size(142, 23);
+            this.commit.TabIndex = 7;
+            this.commit.Text = "Commit Changes";
+            this.commit.UseVisualStyleBackColor = true;
+            // 
+            // nstatus
+            // 
+            this.nstatus.Image = ((System.Drawing.Image)(resources.GetObject("nstatus.Image")));
+            this.nstatus.InitialImage = null;
+            this.nstatus.Location = new System.Drawing.Point(289, 70);
+            this.nstatus.Name = "nstatus";
+            this.nstatus.Size = new System.Drawing.Size(19, 19);
+            this.nstatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.nstatus.TabIndex = 8;
+            this.nstatus.TabStop = false;
+            // 
+            // pstatus
+            // 
+            this.pstatus.Image = ((System.Drawing.Image)(resources.GetObject("pstatus.Image")));
+            this.pstatus.InitialImage = null;
+            this.pstatus.Location = new System.Drawing.Point(289, 156);
+            this.pstatus.Name = "pstatus";
+            this.pstatus.Size = new System.Drawing.Size(19, 19);
+            this.pstatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pstatus.TabIndex = 9;
+            this.pstatus.TabStop = false;
             // 
             // ChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 305);
+            this.ClientSize = new System.Drawing.Size(341, 246);
+            this.Controls.Add(this.pstatus);
+            this.Controls.Add(this.nstatus);
+            this.Controls.Add(this.commit);
+            this.Controls.Add(this.verified);
+            this.Controls.Add(this.verify);
             this.Controls.Add(this.pinText);
             this.Controls.Add(this.nameText);
             this.Controls.Add(this.feedback);
@@ -92,6 +155,8 @@
             this.Name = "ChangePassword";
             this.Text = "ChangePassword";
             this.Load += new System.EventHandler(this.ChangePassword_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nstatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pstatus)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,5 +169,10 @@
         private System.Windows.Forms.Label feedback;
         private System.Windows.Forms.TextBox nameText;
         private System.Windows.Forms.TextBox pinText;
+        private System.Windows.Forms.Label verify;
+        private System.Windows.Forms.TextBox verified;
+        private System.Windows.Forms.Button commit;
+        private System.Windows.Forms.PictureBox nstatus;
+        private System.Windows.Forms.PictureBox pstatus;
     }
 }
--- CG_SK_Lab1/CG_SK_Lab1/ChangePassword.cs
+++ CG_SK_Lab1/CG_SK_Lab1/ChangePassword.cs
@@ -20,6 +20,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace CG_SK_Lab1
 {
@@ -29,10 +30,131 @@ namespace CG_SK_Lab1
         {
             InitializeComponent();
         }
-
+        public bool matchingpwds = false;
         private void ChangePassword_Load(object sender, EventArgs e)
         {
-
+            pstatus.Visible = false;
+            nstatus.Visible = false;
+            matchingpwds = false;
         }
+
+        private void nameText_Leave(object sender, EventArgs e)
+        {
+            check_database_name();
+        }
+
+        private void check_database_name() 
+        {
+            Excel.Application xlApp = new Excel.Application(); //Create New Variable to hold Excel App
+            //string workbookpath = "C:\\Users\\Network Student\\Documents\\GitHub\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";//path
+            string workbookpath = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(workbookpath, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false); //How to access Spreadsheet
+            Excel.Sheets xlsheet = xlWorkBook.Worksheets;                                   // Variable to hold excel Sheets
+            string currentSheet = "Users";                                                  // Set current sheet to be Administrators
+            Excel.Worksheet xlworksheet = (Excel.Worksheet)xlsheet.get_Item(currentSheet);  // Store users into xlworksheet 
+
+            string cellname = "A2";                                                         // First cell with Cadet Name
+            Excel.Range xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);    // store in xlcell
+            string name = xlcell.Value.ToString();                                          // store value in xlcell as a string
+            bool match = false;                                                             // initialize match as not found
+            int i = 2;                                                                      // current row is two
+            while (xlcell.Value != null && !match)                                          // while its not the bottom of the db and a match has not been found
+            {
+
+                if (nameText.Text == name)                                                 // If cadet name is found
+                {
+                    match = true;
+                    nstatus.Visible = true;
+                    nstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\checkmark.png";
+                    
+                }
+                else // Cell is not name
+                {
+                    i++;                                                                    // Move down a row
+                    cellname = "A" + i.ToString();                                          // Set next cell
+                    xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);        // Get name
+                    if (xlcell.Value != null)                                               // if its not empty
+                        name = xlcell.Value.ToString();                                     // set as a string
+                }
+            }
+            if (!match) // Admin Name was not found
+            {
+                nstatus.Visible = true;
+                nstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\error.png";
+               // this.ActiveControl = nameText;                                             // Put Cusor in admin username field
+            }
+        }
+
+        private void verified_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                check_passwords();
+                if (matchingpwds)
+                {
+                    updatepassword();
+                    this.Close();
+                }
+
+            }
+        }
+
+        private void check_passwords()
+        {
+            if (pinText.Text == verified.Text)
+            {
+                pstatus.Visible = true;
+                pstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\checkmark.png";
+                matchingpwds = true;
+            }
+            else
+            {
+                pstatus.Visible = true;
+                pstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\error.png";
+                matchingpwds = false;
+            }
+        }
+        private void updatepassword()
+        {
+            Excel.Application xlApp = new Excel.Application(); //Create New Variable to hold Excel App
+            //string workbookpath = "C:\\Users\\Network Student\\Documents\\GitHub\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";//path
+            string workbookpath = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(workbookpath, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false); //How to access Spreadsheet
+            Excel.Sheets xlsheet = xlWorkBook.Worksheets;                                   // Variable to hold excel Sheets
+            string currentSheet = "Users";                                                  // Set current sheet to be Administrators
+            Excel.Worksheet xlworksheet = (Excel.Worksheet)xlsheet.get_Item(currentSheet);  // Store users into xlworksheet 
+
+            string cellname = "A2";                                                         // First cell with Cadet Name
+            Excel.Range xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);    // store in xlcell
+            string name = xlcell.Value.ToString();                                          // store value in xlcell as a string
+            bool match = false;                                                             // initialize match as not found
+            int i = 2;                                                                      // current row is two
+            while (xlcell.Value != null && !match)                                          // while its not the bottom of the db and a match has not been found
+            {
+
+                if (nameText.Text == name)                                                 // If cadet name is found
+                {
+                    match = true;
+                    nstatus.Visible = true;
+                    nstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\checkmark.png";
+
+                }
+                else // Cell is not name
+                {
+                    i++;                                                                    // Move down a row
+                    cellname = "A" + i.ToString();                                          // Set next cell
+                    xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);        // Get name
+                    if (xlcell.Value != null)                                               // if its not empty
+                        name = xlcell.Value.ToString();                                     // set as a string
+                }
+            }
+            if (!match) // Admin Name was not found
+            {
+                nstatus.Visible = true;
+                nstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\error.png";
+                // this.ActiveControl = nameText;                                             // Put Cusor in admin username field
+            }
+        }
+
     }
 }
