8b46d94c289bad080bdcec3d2bacd2537b5c46f3
ScottKenney <Scott.W.Kenney@uscga.edu>
2013-02-24 17:40:27 -0500

    Enroll Cadet in Progress
    Almost done, need to fix some minor errors and actually add to database

--- CG_SK_Lab1/CG_SK_Lab1/EnrollCadet.Designer.cs
+++ CG_SK_Lab1/CG_SK_Lab1/EnrollCadet.Designer.cs
@@ -35,73 +35,137 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.CloseEnroll = new System.Windows.Forms.Button();
+            this.addcadet = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.directions = new System.Windows.Forms.Label();
+            this.nstatus = new System.Windows.Forms.PictureBox();
+            this.bstatus = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nstatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bstatus)).BeginInit();
             this.SuspendLayout();
             // 
             // nameText
             // 
-            this.nameText.Location = new System.Drawing.Point(69, 75);
+            this.nameText.Location = new System.Drawing.Point(144, 70);
             this.nameText.Name = "nameText";
             this.nameText.Size = new System.Drawing.Size(144, 20);
             this.nameText.TabIndex = 0;
+            this.nameText.Leave += new System.EventHandler(this.nameText_Leave);
             // 
             // bcText
             // 
-            this.bcText.Location = new System.Drawing.Point(69, 139);
+            this.bcText.Location = new System.Drawing.Point(144, 105);
             this.bcText.Name = "bcText";
             this.bcText.Size = new System.Drawing.Size(144, 20);
             this.bcText.TabIndex = 1;
+            this.bcText.Leave += new System.EventHandler(this.bcText_Leave);
             // 
             // pinText
             // 
-            this.pinText.Location = new System.Drawing.Point(69, 205);
+            this.pinText.Location = new System.Drawing.Point(144, 141);
             this.pinText.Name = "pinText";
             this.pinText.Size = new System.Drawing.Size(144, 20);
             this.pinText.TabIndex = 2;
+            this.pinText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pinText_KeyDown);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(75, 50);
+            this.label1.Location = new System.Drawing.Point(55, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.Size = new System.Drawing.Size(87, 20);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Username";
+            this.label1.Text = "Username:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(75, 114);
+            this.label2.Location = new System.Drawing.Point(69, 105);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.Size = new System.Drawing.Size(73, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Barcode";
+            this.label2.Text = "Barcode:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(74, 182);
+            this.label3.Location = new System.Drawing.Point(107, 141);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.Size = new System.Drawing.Size(35, 20);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Pin";
+            this.label3.Text = "Pin:";
             // 
             // CloseEnroll
             // 
-            this.CloseEnroll.Location = new System.Drawing.Point(81, 289);
+            this.CloseEnroll.Location = new System.Drawing.Point(178, 208);
             this.CloseEnroll.Name = "CloseEnroll";
             this.CloseEnroll.Size = new System.Drawing.Size(75, 23);
             this.CloseEnroll.TabIndex = 6;
-            this.CloseEnroll.Text = "Quit";
+            this.CloseEnroll.Text = "Cancel";
             this.CloseEnroll.UseVisualStyleBackColor = true;
+            this.CloseEnroll.Click += new System.EventHandler(this.CloseEnroll_Click);
+            // 
+            // addcadet
+            // 
+            this.addcadet.Location = new System.Drawing.Point(144, 167);
+            this.addcadet.Name = "addcadet";
+            this.addcadet.Size = new System.Drawing.Size(144, 23);
+            this.addcadet.TabIndex = 7;
+            this.addcadet.Text = "Add Cadet";
+            this.addcadet.UseVisualStyleBackColor = true;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(78, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(210, 26);
+            this.title.TabIndex = 8;
+            this.title.Text = "Enroll New Cadet";
+            // 
+            // directions
+            // 
+            this.directions.AutoSize = true;
+            this.directions.Location = new System.Drawing.Point(91, 44);
+            this.directions.Name = "directions";
+            this.directions.Size = new System.Drawing.Size(197, 13);
+            this.directions.TabIndex = 9;
+            this.directions.Text = "Please Enter the Appropriate Information";
+            // 
+            // nstatus
+            // 
+            this.nstatus.Location = new System.Drawing.Point(294, 70);
+            this.nstatus.Name = "nstatus";
+            this.nstatus.Size = new System.Drawing.Size(22, 19);
+            this.nstatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.nstatus.TabIndex = 10;
+            this.nstatus.TabStop = false;
+            this.nstatus.Visible = false;
+            // 
+            // bstatus
+            // 
+            this.bstatus.Location = new System.Drawing.Point(294, 106);
+            this.bstatus.Name = "bstatus";
+            this.bstatus.Size = new System.Drawing.Size(22, 19);
+            this.bstatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bstatus.TabIndex = 11;
+            this.bstatus.TabStop = false;
+            this.bstatus.Visible = false;
             // 
             // EnrollCadet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 374);
+            this.ClientSize = new System.Drawing.Size(369, 253);
+            this.Controls.Add(this.bstatus);
+            this.Controls.Add(this.nstatus);
+            this.Controls.Add(this.directions);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.addcadet);
             this.Controls.Add(this.CloseEnroll);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -112,6 +176,8 @@
             this.Name = "EnrollCadet";
             this.Text = "EnrollCadet";
             this.Load += new System.EventHandler(this.EnrollCadet_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nstatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bstatus)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,5 +192,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button CloseEnroll;
+        private System.Windows.Forms.Button addcadet;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label directions;
+        private System.Windows.Forms.PictureBox nstatus;
+        private System.Windows.Forms.PictureBox bstatus;
     }
 }
--- CG_SK_Lab1/CG_SK_Lab1/EnrollCadet.cs
+++ CG_SK_Lab1/CG_SK_Lab1/EnrollCadet.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace CG_SK_Lab1
 {
@@ -15,10 +16,139 @@ namespace CG_SK_Lab1
         {
             InitializeComponent();
         }
+        public bool newname = false;
+        public bool newcode = false;
+        public bool validpin = false;
 
         private void EnrollCadet_Load(object sender, EventArgs e)
         {
-
+            newname = false;
+            newcode = false;
+            validpin = false;
         }
+
+        private void nameText_Leave(object sender, EventArgs e)
+        {
+            newname = false;
+            if (nameText.Text != "")
+                check_for_name();
+        }
+
+        private void check_for_name()
+        {
+            Excel.Application xlApp = new Excel.Application(); //Create New Variable to hold Excel App
+            //string workbookpath = "C:\\Users\\Network Student\\Documents\\GitHub\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";//path
+            string workbookpath = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(workbookpath, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false); //How to access Spreadsheet
+            Excel.Sheets xlsheet = xlWorkBook.Worksheets;                                   // Variable to hold excel Sheets
+            string currentSheet = "Users";                                                  // Set current sheet to be Administrators
+            Excel.Worksheet xlworksheet = (Excel.Worksheet)xlsheet.get_Item(currentSheet);  // Store users into xlworksheet 
+
+            string cellname = "A2";                                                         // First cell with Cadet Name
+            Excel.Range xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);    // store in xlcell
+            string name = xlcell.Value.ToString();                                          // store value in xlcell as a string
+            bool match = false;                                                             // initialize match as not found
+            int i = 2;                                                                      // current row is two
+            while (xlcell.Value != null && !match)                                          // while its not the bottom of the db and a match has not been found
+            {
+
+                if (nameText.Text == name)                                                 // If cadet name is found
+                {
+                    match = true;
+                    nstatus.Visible = true;
+                    nstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\error.png";
+
+                }
+                else // Cell is not name
+                {
+                    i++;                                                                    // Move down a row
+                    cellname = "A" + i.ToString();                                          // Set next cell
+                    xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);        // Get name
+                    if (xlcell.Value != null)                                               // if its not empty
+                        name = xlcell.Value.ToString();                                     // set as a string
+                }
+            }
+            if (!match) // Cadet Name was not found
+            {
+                nstatus.Visible = true;
+                nstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\checkmark.png";
+                newname = true;
+            }
+        }
+
+        private void bcText_Leave(object sender, EventArgs e)
+        {
+            newcode = false;
+            if (bcText.Text != "")
+                check_for_code();
+        }
+
+        private void check_for_code()
+        {
+            Excel.Application xlApp = new Excel.Application(); //Create New Variable to hold Excel App
+            //string workbookpath = "C:\\Users\\Network Student\\Documents\\GitHub\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";//path
+            string workbookpath = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\testdb";
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(workbookpath, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false); //How to access Spreadsheet
+            Excel.Sheets xlsheet = xlWorkBook.Worksheets;                                   // Variable to hold excel Sheets
+            string currentSheet = "Users";                                                  // Set current sheet to be Administrators
+            Excel.Worksheet xlworksheet = (Excel.Worksheet)xlsheet.get_Item(currentSheet);  // Store users into xlworksheet 
+
+            string cellname = "C2";                                                         // First cell with Cadet Name
+            Excel.Range xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);    // store in xlcell
+            string code = xlcell.Value.ToString();                                          // store value in xlcell as a string
+            bool match = false;                                                             // initialize match as not found
+            int i = 2;                                                                      // current row is two
+            while (xlcell.Value != null && !match)                                          // while its not the bottom of the db and a match has not been found
+            {
+
+                if (bcText.Text == code)                                                 // If cadet name is found
+                {
+                    match = true;
+                    bstatus.Visible = true;
+                    bstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\error.png";
+
+                }
+                else // Cell is not name
+                {
+                    i++;                                                                    // Move down a row
+                    cellname = "C" + i.ToString();                                          // Set next cell
+                    xlcell = (Excel.Range)xlworksheet.get_Range(cellname, cellname);        // Get name
+                    if (xlcell.Value != null)                                               // if its not empty
+                        code = xlcell.Value.ToString();                                     // set as a string
+                }
+            }
+            if (!match) // Cadet Name was not found
+            {
+                bstatus.Visible = true;
+                bstatus.ImageLocation = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\checkmark.png";
+                newcode = true;
+            }
+        }
+
+        private void pinText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (pinText.Text != "")
+                    validpin = true;
+                else
+                    validpin = false;
+                if (validpin && newcode && newname)
+                {
+                    //Add Cadet to filee
+                }
+                else
+                {
+                    directions.Text = "Your Form is not Complete";
+                    directions.ForeColor = Color.Red;
+                }   
+            }
+        }
+
+        private void CloseEnroll_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }
