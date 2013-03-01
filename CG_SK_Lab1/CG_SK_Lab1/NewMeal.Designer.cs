39a8f390a5bbeee52494c754f9bb462eb7936093
pirnu <user@server.fake>
2013-02-28 21:19:27 -0500

    new meal
    added new meal....?

---
+++ CG_SK_Lab1/CG_SK_Lab1/NewMeal.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CG_SK_Lab1
+{
+    partial class NewMeal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BreakRadio = new System.Windows.Forms.RadioButton();
+            this.lunchRadio = new System.Windows.Forms.RadioButton();
+            this.dinnerRadio = new System.Windows.Forms.RadioButton();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BreakRadio
+            // 
+            this.BreakRadio.AutoSize = true;
+            this.BreakRadio.Location = new System.Drawing.Point(43, 24);
+            this.BreakRadio.Name = "BreakRadio";
+            this.BreakRadio.Size = new System.Drawing.Size(70, 17);
+            this.BreakRadio.TabIndex = 0;
+            this.BreakRadio.TabStop = true;
+            this.BreakRadio.Text = "Breakfast";
+            this.BreakRadio.UseVisualStyleBackColor = true;
+            this.BreakRadio.CheckedChanged += new System.EventHandler(this.BreakRadio_CheckedChanged);
+            // 
+            // lunchRadio
+            // 
+            this.lunchRadio.AutoSize = true;
+            this.lunchRadio.Location = new System.Drawing.Point(43, 47);
+            this.lunchRadio.Name = "lunchRadio";
+            this.lunchRadio.Size = new System.Drawing.Size(55, 17);
+            this.lunchRadio.TabIndex = 1;
+            this.lunchRadio.TabStop = true;
+            this.lunchRadio.Text = "Lunch";
+            this.lunchRadio.UseVisualStyleBackColor = true;
+            this.lunchRadio.CheckedChanged += new System.EventHandler(this.lunchRadio_CheckedChanged);
+            // 
+            // dinnerRadio
+            // 
+            this.dinnerRadio.AutoSize = true;
+            this.dinnerRadio.Location = new System.Drawing.Point(43, 70);
+            this.dinnerRadio.Name = "dinnerRadio";
+            this.dinnerRadio.Size = new System.Drawing.Size(56, 17);
+            this.dinnerRadio.TabIndex = 2;
+            this.dinnerRadio.TabStop = true;
+            this.dinnerRadio.Text = "Dinner";
+            this.dinnerRadio.UseVisualStyleBackColor = true;
+            this.dinnerRadio.CheckedChanged += new System.EventHandler(this.dinnerRadio_CheckedChanged);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(43, 128);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(146, 46);
+            this.start.TabIndex = 3;
+            this.start.Text = "Start Meal";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // NewMeal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.dinnerRadio);
+            this.Controls.Add(this.lunchRadio);
+            this.Controls.Add(this.BreakRadio);
+            this.Name = "NewMeal";
+            this.Text = "NewMeal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton BreakRadio;
+        private System.Windows.Forms.RadioButton lunchRadio;
+        private System.Windows.Forms.RadioButton dinnerRadio;
+        private System.Windows.Forms.Button start;
+    }
+}
---
+++ CG_SK_Lab1/CG_SK_Lab1/NewMeal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CG_SK_Lab1
+{
+    public partial class NewMeal : Form
+    {
+        public string meal;
+        public NewMeal()
+        {
+            InitializeComponent();
+        }
+
+        private void BreakRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            meal = "breakfast";
+        }
+
+        private void lunchRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            meal = "lunch";
+        }
+
+        private void dinnerRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            meal = "dinner";
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            //Initializes Database
+            Excel.Application xlApp = new Excel.Application(); //Create New Variable to hold Excel App
+            // string workbookpath = "C:\\Users\\Network Student\\Documents\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\bin\\Debug\\testdb"; //path//path for github
+            string workbookpath = "C:\\Users\\Network Student\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\bin\\debug\\testdb"; //path for Mac-228
+            //string workbookpath = "C:\\Users\\swkenney\\Desktop\\Lab1\\CG_SK_Lab1\\CG_SK_Lab1\\bin\\debug\\testdb"; //Path for Mac-210
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(workbookpath, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false); //How to access Spreadsheet
+            Excel.Sheets xlsheet = xlWorkBook.Worksheets;                                   // Variable to hold excel Sheets
+            string currentSheet = "Attendance";                                             // Set current sheet to be Attendance
+            Excel.Worksheet xlworksheet = (Excel.Worksheet)xlsheet.get_Item(currentSheet);  // Store users into xlworksheet 
+            Excel.Range xlcell = (Excel.Range)xlworksheet.get_Range("E2", "E2");            // store in xlcell
+            string attendance = xlcell.Value.ToString();                                    // store value in xlcell as a string
+            currentSheet = "Totals";
+            xlworksheet = (Excel.Worksheet)xlsheet.get_Item(currentSheet); 
+            //http://social.msdn.microsoft.com/Forums/en-US/csharpgeneral/thread/920180bf-1c84-40f7-b547-ba9532e309cd
+            Excel.Range range = xlworksheet.get_Range("A2:E2", Type.Missing);
+            range.Delete(Excel.XlDeleteShiftDirection.xlShiftUp);
+
+            xlworksheet.Cells[97, 1] = meal;
+            xlworksheet.Cells[97, 2] = DateTime.Today;
+            xlworksheet.Cells[97, 4] = DateTime.Today;
+            xlworksheet.Cells[97, 4].NumberFormat = "General";
+            xlworksheet.Cells[97, 5] = DateTime.Now.DayOfWeek.ToString();
+            xlworksheet.Cells[96, 3] = attendance;
+
+            xlWorkBook.Save();
+            xlWorkBook.Close();
+        }
+    }
+}
